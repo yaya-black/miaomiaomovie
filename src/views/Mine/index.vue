4a6c4df6e6fd78d9a60ba4b7fdcbfd011487f8b6
yaya-black <ji13364006642@163.com>
2020-01-30 22:20:10 +0800

    createComponents

---
+++ src/views/Mine/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="main" >
+       <Header title="我的喵喵"/>
+        <div id="content">
+        <Login/>
+        </div>
+       <TabBar/>
+    </div>
+</template>
+
+<script>
+import Header from '@/components/Header';
+import TabBar from '@/components/TabBar';  //引入Header和TabBar
+import Login from '@/components/Login';
+export default {
+  name:'Mine',
+  components: {
+        Header,
+        TabBar,
+        Login
+    }  //使用局部组件使其生效
+}
+</script>
+
+<style>
+
+</style>
